5c6f3828df015d58e923b137c45af6c19ccf322e
juandiego305 <juandiegocg@ufps.edu.co>
2025-04-04 06:27:10 -0500

    Inicializacion del proyecto en spring

---
+++ src/test/java/com/juancho12/Api_Hotel/ApiHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancho12.Api_Hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
